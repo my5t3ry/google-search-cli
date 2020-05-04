541e3fc1ce4406749b21fb80df9716d890463c95
my5t3ry <sash@mischkonsum.org>
2020-05-04 23:34:17 +0200

    init

--- src/main/java/de/my5t3ry/googlecli/command/AddToBasketCommand.java
+++ src/main/java/de/my5t3ry/googlecli/command/AddToBasketCommand.java
@@ -9,7 +9,7 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class AddToBasketCommand extends AbstractCommand {
     protected AddToBasketCommand() {
-        super("any integer", "add link to basket");
+        super("any integer", "add search hit to basket");
     }
 
     @Override
