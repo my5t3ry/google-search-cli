202383213ec0d5fb54c45c70c48c87b4d70c9e17
my5t3ry <sash@mischkonsum.org>
2020-05-05 01:21:55 +0200

    init

--- build-graal-config.sh
+++ build-graal-config.sh
@@ -4,4 +4,3 @@ export JAVA_HOME=$HOME/.m2/caches/info.picocli.graal/graalvm-ce-java11-19.3.0.2
 rm -rf src/main/resources/graal-config/*
 mvn clean compile assembly:single
 $JAVA_HOME/bin/java -agentlib:native-image-agent=config-output-dir=src/main/resources/graal-config -jar target/google-search-cli.jar
-
