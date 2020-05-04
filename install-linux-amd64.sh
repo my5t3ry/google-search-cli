2953b7af889e9881a7571ad301aeabca8becd352
my5t3ry <sash@mischkonsum.org>
2020-05-05 01:02:57 +0200

    init

---
+++ install-linux-amd64.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+wget https://github.com/my5t3ry/google-search-cli/raw/master/dist/linux-amd64/google-search-cli
+chmod +x google-search-cli
+sudo cp ggoogle-search-cli /usr/local/bin/
