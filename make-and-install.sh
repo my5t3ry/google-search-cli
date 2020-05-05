2257b8bc2f47394f38cb141d97c5c2903ee7481b
my5t3ry <sash@mischkonsum.org>
2020-05-05 04:14:13 +0200

    init

--- make-and-install.sh
+++ make-and-install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 ./install-graal.sh
 export JAVA_HOME=$HOME/.m2/caches/info.picocli.graal/graalvm-ce-java11-19.3.0.2
-mvn -DbuildArgs=--no-server clean verify
+mvn clean verify
 sudo cp target/google-search-cli /usr/local/bin/
